be23f7893e221a02f11725740c8f8cdb3a4675c2
mikeHSU0429 <93478244+mikeHSU0429@users.noreply.github.com>
2022-10-31 19:53:43 +0800

    demo

---
+++ index.js
@@ -0,0 +1,38 @@
+let a =0
+let b = 0
+let homePoint = document.getElementById("homePoint")
+let guestPoint = document.getElementById("guestPoint")
+
+function addOneH(){
+      
+        
+        a+=1
+        homePoint.textContent=a
+}
+function addOneG(){
+      
+        b+=1
+        guestPoint.textContent=b
+}
+
+
+function addTwoH(){
+        a+=2
+        homePoint.textContent=a
+    }
+function addTwoG(){
+        b+=2
+        guestPoint.textContent=b
+    }
+   
+
+function addThreeH(){
+        a+=3
+        homePoint.textContent=a
+    }
+function addThreeG(){
+        b+=3
+        guestPoint.textContent=b
+    }
+
+
